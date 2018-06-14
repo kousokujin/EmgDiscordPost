cc3aaf0f03040113979c0a6b8adc5f7ab5168b58
kousokujin <kousokujin@outlook.jp>
2018-06-14 14:35:52 +0900

    プロジェクト ファイルを追加します。

---
+++ EmgDiscordPost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmgDiscordPost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
