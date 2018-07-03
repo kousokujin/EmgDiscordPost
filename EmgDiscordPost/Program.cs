d114f3f9883c438d20d4eef841a4f382d16cef48
kousokujin <kousokujin@outlook.jp>
2018-07-03 23:40:13 +0900

    メインとなる何かを作成

--- EmgDiscordPost/Program.cs
+++ EmgDiscordPost/Program.cs
@@ -17,76 +17,12 @@ namespace EmgDiscordPost
 
             Console.Write("Token:");
             string token = Console.ReadLine();
-            ulong id = 0;
+            ulong id = 4;
 
-            //覇者の紋章
-            IChpDataRead chpData = new PostgreSQL_ChpRead(address, database, user, pass);
-            IChpTimeDB timeData = new PostgreSQL_chp_confDB(address, database, user, pass);
-            chpDB chpDBcon = new chpDB(chpData, timeData);
-
-            IemgPost post = new emgPostToDiscord(token, id, botname);
-            ChpPostController postCon = new ChpPostController(post);
-
-            ChpController chp = new ChpController(chpDBcon, postCon);
-            chp.initDB();
-            DateTime notify = DateTime.Now + new TimeSpan(0, 0, 30);
-            //timeData.addChpTable((int)notify.DayOfWeek, notify.Hour, notify.Minute, notify.Second);
-            //chp.reloadNotifyTime();
-
-            //緊急クエスト
-            IemgPost emgPost = new emgPostToDiscord(token, id, botname);
-            IEmgDBRead emgDBread = new PostgreSQL_EmgRead(address, database, user, pass);
-            IDBConfig dbConfig = new PostgreSQL_configLoader(address, database, user, pass);
-            EmgPostController emgPostCon = new EmgPostController(emgPost);
-            EmgDBGet emgDB = new EmgDBGet(emgDBread);
-            EmgController emgCon = new EmgController(emgPostCon, emgDB, dbConfig);
-            dbConfig.droptable();
-            dbConfig.createtable();
-
-            //緊急クエストエントリー
-            IJoinMemberDB joinDatabase = new PostgreSQL_joinDB(address, database, user, pass);
-            IjoinPost joinDiscordCient = new joinPostDiscord(token, id, botname);
-            joinDB jDB = new joinDB(joinDatabase, emgDBread);
-            joinServiceController joinDiscord = new joinServiceController(joinDiscordCient);
-            jDB.initDB();
-            JoinController join = new JoinController(jDB, joinDiscord, dbConfig);
-
-            //その他
-            IPostService discord = new DiscordService(token, id, botname);
-            List<AbstractServiceController> lstService = new List<AbstractServiceController>();
-            lstService.Add(postCon);
-            lstService.Add(emgPostCon);
-            lstService.Add(joinDiscord);
-            DefaultDiscordController defController = new DefaultDiscordController(discord, lstService);
+            MainCON con = new MainCON(address, database, user, pass, botname, id, token);
+            con.FirstMigration();
 
             Console.ReadLine();
-        }
-
-        static void Recive(object sender,EventArgs data)
-        {
-            if (data is ReceiveData)
-            {
-                ReceiveData d = data as ReceiveData;
-                Console.WriteLine("受信:{0}", d.content);
-            }
-        }
-
-        static void Replay(object sender, EventArgs data)
-        {
-            if (data is ReceiveData)
-            {
-                ReceiveData d = data as ReceiveData;
-                Console.WriteLine("リプライ:{0}", d.content);
-            }
-        }
-
-        static void cancelEv(object sender, EventArgs data)
-        {
-            if (data is ReceiveData)
-            {
-                ReceiveData j = data as ReceiveData;
-                Console.WriteLine("Cancel:{0}", j.Author);
-            }
         }
     }
 }
